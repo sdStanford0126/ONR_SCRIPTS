326137f131093a9abd8146bea8bfaf0ec11709cd
Steven Dai <sd0126@stanford.edu>
2026-05-06 02:08:39 -0400

    BL_ax_prof_post.py, added plots, added y+ calculation, now includes limited streamwise averaging, loglog and semilogx plotting and are plotting law of the wall style plots

--- python_postproc/BL_ax_prof_post.py
+++ python_postproc/BL_ax_prof_post.py
@@ -26,7 +26,6 @@ xs = np.linspace(-1,0,5)
 #new version
 
 y = np.arange(-(1.0 - delta / 2.0), (1.0 - delta / 2.0) + delta * 0.5, delta)
-
 #thermodymaics reference for Power-Law viscosity
 mu_ref = 3.26788998e-6
 T_ref = 1.0
@@ -153,6 +152,7 @@ def extract_data(x,posName,DataName_fmt,tid_str,tid_end,dt,max_workers=None):
     p  = np.zeros((Nt,Ny,Nz))
     rho  = np.zeros((Nt,Ny,Nz))
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        print("active threads: ", executor._max_workers)
         futures = {executor.submit(_load_timestep, DataName_fmt, tid, ind, X_ind, Ny, Nz): i
                    for i, tid in enumerate(tids)}
         for future in tqdm(as_completed(futures), total=Nt, desc=f"x={x:.3f}"):
@@ -170,7 +170,7 @@ def extract_data(x,posName,DataName_fmt,tid_str,tid_end,dt,max_workers=None):
 #u,v,w,p,rho
 #we would like to at least assemble the following:
 #avg(u) profile, avg(u'v'), avg(u'w'), 
-def plotTurbProf_C(u,v,w,rho,p,xc,y,z, out_dir = "./",eps=delta/2.0, fig_offset=0):
+def plotTurbProf_C(u,v,w,rho,p,xc,y,z, out_dir = "./",eps=delta/1.9, fig_offset=0):
     """
     takes in time history and the 1d y,z vector and produces the following profiles at given streamwise station x = xc
     u_avg over y centerline (major axis)
@@ -180,14 +180,42 @@ def plotTurbProf_C(u,v,w,rho,p,xc,y,z, out_dir = "./",eps=delta/2.0, fig_offset=
     tke over y centerline (major axis)
     tke over z centerline (minor axis)
     """
-    #find centerline 
-    y_ind = np.where(np.abs(y) < eps)
-    z_ind = np.where(np.abs(z) < eps)
-    y_ind = y_ind[0]
-    z_ind = z_ind[0]
+    #do averging across an region (see axial line profiles)
+    #definte limits
+    y_lim = 0.85  #+/-y_lim
+    z_lim = 0.35  #+/-z_lim
+    #find bounds    
+    y_ind_lo = np.where(((-y_lim - eps) < y) & (y < (-y_lim + eps)))
+    y_ind_hi = np.where((( y_lim - eps) < y) & (y < ( y_lim + eps)))
+    z_ind_lo = np.where(((-z_lim - eps) < z) & (z < (-z_lim + eps)))
+    z_ind_hi = np.where((( z_lim - eps) < z) & (z < ( z_lim + eps)))
+
+    
+    y_ind_lo = y_ind_lo[0][0]
+    y_ind_hi = y_ind_hi[0][0]
+    z_ind_lo = z_ind_lo[0][0]
+    z_ind_hi = z_ind_hi[0][0]
+
+    print(y_ind_lo)
+    print(y_ind_hi) 
+    print(z_ind_lo)
+    print(z_ind_hi) 
+
+    y_ind = np.arange(y_ind_lo, y_ind_hi+1)
+    z_ind = np.arange(z_ind_lo, z_ind_hi+1)
+    #preivous centerline implementation
+    #y_ind = np.where(np.abs(y) < eps)
+    #z_ind = np.where(np.abs(z) < eps)
+    #y_ind = y_ind[0]
+    #z_ind = z_ind[0]
+    
+    
+    Nz = np.size(z)
+
+
     #debug (print loc)
-    print(y[y_ind])
-    print(z[z_ind])
+    #print(y[y_ind])
+    #print(z[z_ind])
     
     #get time averages
     u_avg  = np.mean(u,axis=0)
@@ -207,18 +235,30 @@ def plotTurbProf_C(u,v,w,rho,p,xc,y,z, out_dir = "./",eps=delta/2.0, fig_offset=
     #print("u_avg_z_size: ",u_avg_z.size)
     #print("y: ",z.size)
 
+    u_avg_y = np.squeeze(np.mean(u_avg_y,axis=1))
+    u_avg_z = np.squeeze(np.mean(u_avg_z,axis=0))
+
     rho_avg_y = np.squeeze(rho_avg[:,z_ind])
     rho_avg_z = np.squeeze(rho_avg[y_ind,:])
+
+    rho_avg_y = np.squeeze(np.mean(rho_avg_y,axis=1))
+    rho_avg_z = np.squeeze(np.mean(rho_avg_z,axis=0))
 
     p_avg_y = np.squeeze(p_avg[:,z_ind])
     p_avg_z = np.squeeze(p_avg[y_ind,:])
 
+    p_avg_y = np.squeeze(np.mean(p_avg_y,axis=1))
+    p_avg_z = np.squeeze(np.mean(p_avg_z,axis=0))
+
+    print("shape of rho_avg_y: ", np.shape(rho_avg_y))
+    print("shape of p_avg_y: ", np.shape(p_avg_y))
     T_avg_y = p_avg_y/rho_avg_y
     T_avg_z = p_avg_z/rho_avg_z
-
+    print("Shape of T_avg_y: ", np.shape(T_avg_y))
     mu_avg_y = muCalc(T_avg_y)
     mu_avg_z = muCalc(T_avg_z)
 
+    print("shape of mu_avg_y: ",np.shape(mu_avg_y))
     mu_w_y = (mu_avg_y[0]+mu_avg_y[-1])/2.0
     mu_w_z = (mu_avg_z[0]+mu_avg_z[-1])/2.0
     dy = np.abs(y[1]-y[0])
@@ -253,15 +293,31 @@ def plotTurbProf_C(u,v,w,rho,p,xc,y,z, out_dir = "./",eps=delta/2.0, fig_offset=
     #build fluctuation
     ufvf_avg = np.mean(uf*vf,axis=0)
     ufvf_avg_y = np.squeeze(ufvf_avg[:,z_ind])
+    ufvf_avg_y = np.mean(ufvf_avg_y,axis=1)
 
     ufwf_avg = np.mean(uf*wf, axis=0)
     ufwf_avg_z = np.squeeze(ufwf_avg[y_ind,:])
-
+    ufwf_avg_z = np.mean(ufwf_avg_z,axis=0)
 
     PlotName_fmt = "{}_{}.png"
     PlotName_fmt = os.path.join(out_dir, PlotName_fmt)
+    LogLogName_fmt = "{}_{}_loglog.png"
+    LogLogName_fmt = os.path.join(out_dir, LogLogName_fmt)
+    SemiLogXName_fmt = "{}_{}_loglog.png"
+    SemiLogXName_fmt = os.path.join(out_dir, SemiLogXName_fmt)
+    ## y_plus calc
+    y_p = _calcYplus(y+1.0,u_tau_y,mu_w_y/rhoy_w)
+    z_p = _calcYplus(z+0.5,u_tau_z,mu_w_z/rhoz_w) 
 
-    fig_count = 7
+    y_p = y_p
+    print("first ten elements of y_p: ", y_p[:10])
+    z_p = z_p
+    print("first ten elements of z_p", z_p[:10])
+    u_p_y = u_avg_y / u_tau_y    
+    u_p_z = u_avg_z / u_tau_z    
+    ## ploting happending below
+    
+    fig_count = 9
     #u_avg_y
     fig_id = 0 + fig_count*fig_offset
     var_label = "u_avg"
@@ -269,7 +325,6 @@ def plotTurbProf_C(u,v,w,rho,p,xc,y,z, out_dir = "./",eps=delta/2.0, fig_offset=
     PlotName = PlotName_fmt.format(var_label,side_label)
     plotProfile(xc,y,u_avg_y,r"$\overline{u}$", side_label,PlotName,fig_id)
 
-    
     #u_avg_z
     fig_id = 1 + fig_count*fig_offset
     var_label = "u_avg"
@@ -293,21 +348,65 @@ def plotTurbProf_C(u,v,w,rho,p,xc,y,z, out_dir = "./",eps=delta/2.0, fig_offset=
 
     #ufvf_avg_y
     fig_id = 4+ fig_count*fig_offset
-    var_label = "ufvf_avg_div_u_tau_y"
+    var_label = "ufvf_avg_div_u_tau_y_2"
     side_label = "y"
     PlotName = PlotName_fmt.format(var_label,side_label)
-    plotProfile(xc,y,ufvf_avg_y/u_tau_y,r"$\frac{\overline{u'v'}}{u_{\tau,y}}$", side_label,PlotName,fig_id)
+    plotProfile(xc,y,ufvf_avg_y/(u_tau_y**2),r"$\frac{\overline{u'v'}}{u_{\tau,y}}$", side_label,PlotName,fig_id)
 
     #ufwf_avg_z
     fig_id = 5 + fig_count*fig_offset
-    var_label = "ufwf_avg_div_u_tau_z"
+    var_label = "ufwf_avg_div_u_tau_z_2"
     side_label = "z"
     PlotName = PlotName_fmt.format(var_label,side_label)
-    plotProfile(xc,z,ufwf_avg_z/u_tau_z,r"$\frac{\overline{u'w'}}{u_{\tau,z}}$", side_label,PlotName,fig_id)
+    plotProfile(xc,z,ufwf_avg_z/(u_tau_z**2),r"$\frac{\overline{u'w'}}{u_{\tau,z}}$", side_label,PlotName,fig_id)
+
+
+    #u_avg_y law of the wall
+    fig_id = 6 + fig_count*fig_offset
+    var_label = "u+"
+    side_label = "y+"
+    PlotName = SemiLogXName_fmt.format(var_label,side_label)
+    plotSemiLogXProfile(xc,u_p_y[:Ny//2],y_p[:Ny//2],side_label, r"$\overline{u}_+$",PlotName,fig_id)
+
+    #u_avg_z law of the wall
+    fig_id = 7 + fig_count*fig_offset
+    var_label = "u+"
+    side_label = "z+"
+    PlotName = SemiLogXName_fmt.format(var_label,side_label)
+    plotSemiLogXProfile(xc,u_p_z[:Nz//2],z_p[:Nz//2],side_label,r"$\overline{u}_+$",PlotName,fig_id)
+
+
+def _calcYplus(y,u_tau,nu):
+    #Return coordinate in y+ units
+    #INPUT: y: coordinate
+    #       u_tau: friction velocity
+    #       nu: kinematic viscosity
+    #OUTPUT:  y in y+ units
+    # y+ def: y+ = y * u_tau/nu 
+    return y*u_tau/nu
 
 def plotProfile(xc,pos,var,var_label:str,side_label:str,PlotName: str,fig_id):
     plt.figure(fig_id)
     plt.plot(var,pos, label = "x = %.2f" % xc)
+    plt.xlabel(var_label)
+    plt.ylabel(side_label)
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(PlotName, dpi = 300, bbox_inches="tight")
+
+def plotLogLogProfile(xc,pos,var,var_label:str,side_label:str,PlotName: str,fig_id):
+    plt.figure(fig_id)
+    plt.loglog(var,pos, label = "x = %.2f" % xc)
+    plt.xlabel(var_label)
+    plt.ylabel(side_label)
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(PlotName, dpi = 300, bbox_inches="tight")
+
+
+def plotSemiLogXProfile(xc,pos,var,var_label:str,side_label:str,PlotName: str,fig_id):
+    plt.figure(fig_id)
+    plt.semilogx(var,pos, label = "x = %.2f" % xc)
     plt.xlabel(var_label)
     plt.ylabel(side_label)
     plt.legend()
@@ -321,32 +420,46 @@ def evalMfAvg(mf,z,y):
     return mf_tot
 
 
-def plotAxialProf(xc,var,z,y,var_label:str,out_dir):
+def plotAxialProf(xc,var,z,y,var_label:str,out_dir,var_lim=None):
     PlotName_fmt = "{}_x_{}.png"
     PlotName_fmt = os.path.join(out_dir,PlotName_fmt)
     PlotName = PlotName_fmt.format(var_label,xc) 
     plt.figure()
-    plt.pcolormesh(y,z,np.transpose(var),shading='nearest')
+    if var_lim ==None:
+        plt.pcolormesh(y,z,np.transpose(var),shading='nearest')
+    else:
+        #if var_lim is defined
+        plt.pcolormesh(y,z,np.transpose(var),shading='nearest',vmin=var_lim[0],vmax=var_lim[1])
     plt.xlabel("y")
     plt.ylabel("z")
     plt.colorbar()
     plt.title(var_label)
     plt.tight_layout()
     plt.axis('equal')
+
+    plt.axvline(x= 0.85,color='r',linestyle='--')
+    plt.axvline(x=-0.85,color='r',linestyle='--')
+    plt.axhline(y= 0.35,color='c',linestyle='-.')
+    plt.axhline(y=-0.35,color='c',linestyle='-.')
+
     plt.savefig(PlotName,dpi=300,bbox_inches="tight")
     
 def main():
         #these are for test purposes
-    TBL_test_cases = ["0.0125", "0.01875", "0.025"]
-    tid_str_cases  = [40000, 40000, 130000]
-    tid_end_cases  = [100000, 100000, 190000]
+    #TBL_test_cases = ["0.0125", "0.01875", "0.025"]
+    #tid_str_cases  = [40000, 40000, 130000]
+    #tid_end_cases  = [100000, 100000, 190000]
 
-    #TBL_test_cases = ["0.025"]
-    #tid_str_cases  = [130000]
-    #tid_end_cases  = [160000]
+    TBL_test_cases = ["0.025"]
+    tid_str_cases  = [155000]
+    tid_end_cases  = [160000]
     data_dir_fmt = "/anvil/scratch/x-sdai/BL_test_baseline_{:s}/pcprobe_int_axprof"
     out_dir_fmt = "/anvil/scratch/x-sdai/BL_post_proc/BL_{:s}"
+     
+    cpus = 4
+    print("assigned core count: ", cpus)
     for i, case in enumerate(TBL_test_cases):
+        plt.close('all')
         print("Now processing for TBL case: ", case)
         data_dir = data_dir_fmt.format(case)
         print(xs)
@@ -359,7 +472,8 @@ def main():
         tid_str = tid_str_cases[i]
         tid_end = tid_end_cases[i]
         for x in xs:
-            u,v,w,p,rho,mf= extract_data(x,posName,DataName_fmt,tid_str,tid_end,50)
+            #cpus = int(os.environ.get('SLURM_NTASK', 1))
+            u,v,w,p,rho,mf= extract_data(x,posName,DataName_fmt,tid_str,tid_end,50,max_workers=cpus)
             z_max = z_lim(x)
             #print(y)
             T = p/rho
@@ -368,7 +482,8 @@ def main():
             print("mass flow at x=%.2f is mf = %.2f " % (x,evalMfAvg(mf,z,y)))
             plotTurbProf_C(u,v,w,rho,p,x,y,z,out_dir = out_dir)
             plotAxialProf(x,np.mean(u,axis=0),z,y,"u_avg",out_dir)
-            plotAxialProf(x,np.mean(u,axis=0)/np.mean(c,axis=0),z,y,"Mach_u",out_dir)
+            plotAxialProf(x,np.mean(u,axis=0)/np.mean(c,axis=0),z,y,"Mach_u",out_dir,var_lim=(0,1.5))
+
             
 
     #load baseline h5 make the same plots based using the same functions
