c20d121bfc9339a5f5b7e1ff424a4a99a9327757
Steven Dai <sd0126@stanford.edu>
2026-05-21 12:56:28 -0400

    added output function for most turbulent statistics, modifying BL_ax_prof_post.py for the pcprobes_noz_int

--- python_postproc/BL_ax_prof_post.py
+++ python_postproc/BL_ax_prof_post.py
@@ -21,9 +21,11 @@ z_str = 0.42498737
 z_end = 0.5
 slope = (z_end-z_str)/(x_end-x_str)
 
-#old versoin
-xs = np.linspace(-1,0,5)
-#new version
+#old version (used for BL_test_<roughness_height>)
+#xs = np.linspace(-1,0,5)
+#new version (used for AR2_base_<*>)
+Nx = 101 
+xs = np.linspace(-1,0,Nx) #direct defined from write_BL_interior_probes.py
 
 y = np.arange(-(1.0 - delta / 2.0), (1.0 - delta / 2.0) + delta * 0.5, delta)
 #thermodymaics reference for Power-Law viscosity
@@ -303,7 +305,7 @@ def plotTurbProf_C(u,v,w,rho,p,xc,y,z, out_dir = "./",eps=delta/1.9, fig_offset=
     PlotName_fmt = os.path.join(out_dir, PlotName_fmt)
     LogLogName_fmt = "{}_{}_loglog.png"
     LogLogName_fmt = os.path.join(out_dir, LogLogName_fmt)
-    SemiLogXName_fmt = "{}_{}_loglog.png"
+    SemiLogXName_fmt = "{}_{}_semilogx.png"
     SemiLogXName_fmt = os.path.join(out_dir, SemiLogXName_fmt)
     ## y_plus calc
     y_p = _calcYplus(y+1.0,u_tau_y,mu_w_y/rhoy_w)
@@ -332,33 +334,33 @@ def plotTurbProf_C(u,v,w,rho,p,xc,y,z, out_dir = "./",eps=delta/1.9, fig_offset=
     PlotName = PlotName_fmt.format(var_label,side_label)
     plotProfile(xc,z,u_avg_z,r"$\overline{u}$", side_label,PlotName,fig_id)
 
-    #ufvf_avg_y
-    fig_id = 2+ fig_count*fig_offset
-    var_label = "ufvf_avg"
-    side_label = "y"
-    PlotName = PlotName_fmt.format(var_label,side_label)
-    plotProfile(xc,y,ufvf_avg_y,r"$\overline{u'v'}$", side_label,PlotName,fig_id)
+    ##ufvf_avg_y
+    #fig_id = 2+ fig_count*fig_offset
+    #var_label = "ufvf_avg"
+    #side_label = "y"
+    #PlotName = PlotName_fmt.format(var_label,side_label)
+    #plotProfile(xc,y,ufvf_avg_y,r"$\overline{u'v'}$", side_label,PlotName,fig_id)
 
-    #ufwf_avg_z
-    fig_id = 3 + fig_count*fig_offset
-    var_label = "ufwf_avg"
-    side_label = "z"
-    PlotName = PlotName_fmt.format(var_label,side_label)
-    plotProfile(xc,z,ufwf_avg_z,r"$\overline{u'w'}$", side_label,PlotName,fig_id)
+    ##ufwf_avg_z
+    #fig_id = 3 + fig_count*fig_offset
+    #var_label = "ufwf_avg"
+    #side_label = "z"
+    #PlotName = PlotName_fmt.format(var_label,side_label)
+    #plotProfile(xc,z,ufwf_avg_z,r"$\overline{u'w'}$", side_label,PlotName,fig_id)
 
     #ufvf_avg_y
     fig_id = 4+ fig_count*fig_offset
     var_label = "ufvf_avg_div_u_tau_y_2"
-    side_label = "y"
+    side_label = "y+"
     PlotName = PlotName_fmt.format(var_label,side_label)
-    plotProfile(xc,y,ufvf_avg_y/(u_tau_y**2),r"$\frac{\overline{u'v'}}{u_{\tau,y}}$", side_label,PlotName,fig_id)
+    plotSemiLogXProfile(xc,ufvf_avg_y[:Ny//2]/(u_tau_y**2),y_p[:Ny//2],side_label, r"$\frac{\overline{u'v'}}{u_{\tau,y}}$",PlotName,fig_id)
 
     #ufwf_avg_z
     fig_id = 5 + fig_count*fig_offset
     var_label = "ufwf_avg_div_u_tau_z_2"
-    side_label = "z"
+    side_label = "z+"
     PlotName = PlotName_fmt.format(var_label,side_label)
-    plotProfile(xc,z,ufwf_avg_z/(u_tau_z**2),r"$\frac{\overline{u'w'}}{u_{\tau,z}}$", side_label,PlotName,fig_id)
+    plotSemiLogXProfile(xc,ufwf_avg_z[:Nz//2]/(u_tau_z**2),z_p[:Nz//2],side_label, r"$\frac{\overline{u'w'}}{u_{\tau,z}}$",PlotName,fig_id)
 
 
     #u_avg_y law of the wall
@@ -374,6 +376,132 @@ def plotTurbProf_C(u,v,w,rho,p,xc,y,z, out_dir = "./",eps=delta/1.9, fig_offset=
     side_label = "z+"
     PlotName = SemiLogXName_fmt.format(var_label,side_label)
     plotSemiLogXProfile(xc,u_p_z[:Nz//2],z_p[:Nz//2],side_label,r"$\overline{u}_+$",PlotName,fig_id)
+
+
+
+
+def CalcTurbProf(u, v, w, rho, p, xc, y, z, eps=delta/1.9):
+    """
+    Compute boundary-layer turbulence profiles at streamwise station xc.
+
+    Mirrors plotTurbProf_C but returns all plotted quantities as a dict
+    instead of saving figures.
+
+    Returns
+    -------
+    dict with keys:
+        y, u_avg_y           : mean-u profile on major (y) axis
+        z, u_avg_z           : mean-u profile on minor (z) axis
+        y_half, ufvf_avg_y_norm  : u'v'/u_tau_y^2, wall-side half-profile
+        z_half, ufwf_avg_z_norm  : u'w'/u_tau_z^2, wall-side half-profile
+        y_p, u_p_y           : law-of-the-wall profile (y side, wall units)
+        z_p, u_p_z           : law-of-the-wall profile (z side, wall units)
+        u_tau_y, u_tau_z     : friction velocities
+        tauy_w, tauz_w       : wall shear stresses
+        rhoy_w, rhoz_w       : wall densities
+        mu_w_y,  mu_w_z      : wall dynamic viscosities
+        rho_avg_y, rho_avg_z : spanwise-averaged mean density profiles
+        T_avg_y,   T_avg_z   : spanwise-averaged mean temperature profiles
+    """
+    y_lim_val = 0.85
+    z_lim_val = 0.35
+
+    y_ind_lo = np.where(((-y_lim_val - eps) < y) & (y < (-y_lim_val + eps)))[0][0]
+    y_ind_hi = np.where((( y_lim_val - eps) < y) & (y < ( y_lim_val + eps)))[0][0]
+    z_ind_lo = np.where(((-z_lim_val - eps) < z) & (z < (-z_lim_val + eps)))[0][0]
+    z_ind_hi = np.where((( z_lim_val - eps) < z) & (z < ( z_lim_val + eps)))[0][0]
+
+    y_ind = np.arange(y_ind_lo, y_ind_hi + 1)
+    z_ind = np.arange(z_ind_lo, z_ind_hi + 1)
+
+    Ny_loc = np.size(y)
+    Nz_loc = np.size(z)
+
+    u_avg   = np.mean(u,   axis=0)
+    v_avg   = np.mean(v,   axis=0)
+    w_avg   = np.mean(w,   axis=0)
+    rho_avg = np.mean(rho, axis=0)
+    p_avg   = np.mean(p,   axis=0)
+
+    uf = u - u_avg
+    vf = v - v_avg
+    wf = w - w_avg
+
+    u_avg_y = np.squeeze(u_avg[:, z_ind])
+    u_avg_y = np.squeeze(np.mean(u_avg_y, axis=1))
+    u_avg_z = np.squeeze(u_avg[y_ind, :])
+    u_avg_z = np.squeeze(np.mean(u_avg_z, axis=0))
+
+    rho_avg_y = np.squeeze(rho_avg[:, z_ind])
+    rho_avg_y = np.squeeze(np.mean(rho_avg_y, axis=1))
+    rho_avg_z = np.squeeze(rho_avg[y_ind, :])
+    rho_avg_z = np.squeeze(np.mean(rho_avg_z, axis=0))
+
+    p_avg_y = np.squeeze(p_avg[:, z_ind])
+    p_avg_y = np.squeeze(np.mean(p_avg_y, axis=1))
+    p_avg_z = np.squeeze(p_avg[y_ind, :])
+    p_avg_z = np.squeeze(np.mean(p_avg_z, axis=0))
+
+    T_avg_y = p_avg_y / rho_avg_y
+    T_avg_z = p_avg_z / rho_avg_z
+    mu_avg_y = muCalc(T_avg_y)
+    mu_avg_z = muCalc(T_avg_z)
+
+    mu_w_y = (mu_avg_y[0] + mu_avg_y[-1]) / 2.0
+    mu_w_z = (mu_avg_z[0] + mu_avg_z[-1]) / 2.0
+
+    dy = np.abs(y[1] - y[0])
+    dz = np.abs(z[1] - z[0])
+    dudy_w = (-3*u_avg_y[0] + 4*u_avg_y[1] - u_avg_y[2]) / (2*dy)
+    dudz_w = (-3*u_avg_z[0] + 4*u_avg_z[1] - u_avg_z[2]) / (2*dz)
+
+    tauy_w = mu_w_y * dudy_w
+    tauz_w = mu_w_z * dudz_w
+    rhoy_w = (rho_avg_y[0] + rho_avg_y[-1]) / 2.0
+    rhoz_w = (rho_avg_z[0] + rho_avg_z[-1]) / 2.0
+
+    u_tau_y = np.sqrt(tauy_w / rhoy_w)
+    u_tau_z = np.sqrt(tauz_w / rhoz_w)
+
+    ufvf_avg   = np.mean(uf * vf, axis=0)
+    ufvf_avg_y = np.squeeze(ufvf_avg[:, z_ind])
+    ufvf_avg_y = np.mean(ufvf_avg_y, axis=1)
+
+    ufwf_avg   = np.mean(uf * wf, axis=0)
+    ufwf_avg_z = np.squeeze(ufwf_avg[y_ind, :])
+    ufwf_avg_z = np.mean(ufwf_avg_z, axis=0)
+
+    y_p = _calcYplus(y + 1.0, u_tau_y, mu_w_y / rhoy_w)
+    z_p = _calcYplus(z + 0.5, u_tau_z, mu_w_z / rhoz_w)
+    u_p_y = u_avg_y / u_tau_y
+    u_p_z = u_avg_z / u_tau_z
+
+    return {
+        "y":       y,
+        "u_avg_y": u_avg_y,
+        "z":       z,
+        "u_avg_z": u_avg_z,
+        "y_half":          y[:Ny_loc // 2],
+        "ufvf_avg_y_norm": ufvf_avg_y[:Ny_loc // 2] / u_tau_y**2,
+        "z_half":          z[:Nz_loc // 2],
+        "ufwf_avg_z_norm": ufwf_avg_z[:Nz_loc // 2] / u_tau_z**2,
+        "y_p":   y_p[:Ny_loc // 2],
+        "u_p_y": u_p_y[:Ny_loc // 2],
+        "z_p":   z_p[:Nz_loc // 2],
+        "u_p_z": u_p_z[:Nz_loc // 2],
+        "u_tau_y": u_tau_y,
+        "u_tau_z": u_tau_z,
+        "tauy_w":  tauy_w,
+        "tauz_w":  tauz_w,
+        "rhoy_w":  rhoy_w,
+        "rhoz_w":  rhoz_w,
+        "mu_w_y":  mu_w_y,
+        "mu_w_z":  mu_w_z,
+        "rho_avg_y": rho_avg_y,
+        "rho_avg_z": rho_avg_z,
+        "T_avg_y":   T_avg_y,
+        "T_avg_z":   T_avg_z,
+    }
 
 
 def _calcYplus(y,u_tau,nu):
@@ -419,7 +547,6 @@ def evalMfAvg(mf,z,y):
     mf_tot = np.mean(mf_tot) 
     return mf_tot
 
-
 def plotAxialProf(xc,var,z,y,var_label:str,out_dir,var_lim=None):
     PlotName_fmt = "{}_x_{}.png"
     PlotName_fmt = os.path.join(out_dir,PlotName_fmt)
@@ -443,20 +570,41 @@ def plotAxialProf(xc,var,z,y,var_label:str,out_dir,var_lim=None):
     plt.axhline(y=-0.35,color='c',linestyle='-.')
 
     plt.savefig(PlotName,dpi=300,bbox_inches="tight")
-    
-def main():
-        #these are for test purposes
-    TBL_test_cases = ["0.0125", "0.01875", "0.025"]
-    tid_str_cases  = [40000, 40000, 130000]
-    tid_end_cases  = [100000, 100000, 190000]
 
+def calcDel99():
+    #TODO: calculate delta_99
+    pass
+
+def calcDelStr():
+    #TODO: calculate displacement thickness (delta_star)
+    pass
+
+def calcTheta():
+    #TODO: calculate momentum thickness (theta)
+    pass
+
+def main():
+    #these are for test purposes
+    #General BL test cases
+    #TBL_test_cases = ["0.0125", "0.01875", "0.025"]
+    #tid_str_cases  = [40000, 40000, 130000]
+    #tid_end_cases  = [100000, 100000, 190000]
+
+    #BL test debug    
     #TBL_test_cases = ["0.025"]
     #tid_str_cases  = [155000]
     #tid_end_cases  = [160000]
-    data_dir_fmt = "/anvil/scratch/x-sdai/BL_test_baseline_{:s}/pcprobe_int_axprof"
-    out_dir_fmt = "/anvil/scratch/x-sdai/BL_post_proc/BL_{:s}"
-     
-    cpus = int(os.getenv("SLURM_NTASKS"))
+    #data_dir_fmt = "/anvil/scratch/x-sdai/BL_test_baseline_{:s}/pcprobe_int_axprof"
+    #out_dir_fmt = "/anvil/scratch/x-sdai/BL_post_proc/BL_{:s}"
+    
+    
+    TBL_test_cases = ["151M_akhil_restart"]
+    data_dir_fmt = "/anvil/scratch/x-sdai/AR2_base_{:s}/pcprobes_noz_int"
+    out_dir = "./test_Baseline_BL"
+
+
+    cpus = 4
+    #cpus = int(os.getenv("SLURM_NTASKS"))
     print("assigned core count: ", cpus)
     for i, case in enumerate(TBL_test_cases):
         plt.close('all')
@@ -465,7 +613,7 @@ def main():
         print(xs)
         z_max = z_lim(0)
         z =np.arange(-(z_max - delta / 2.0), (z_max - delta / 2.0) + delta * 0.5, delta) 
-        out_dir = out_dir_fmt.format(case)
+        #out_dir = out_dir_fmt.format(case)
         posName = os.path.join(data_dir,"int_axprof.pxyz")
         fname_fmt = "int_axprof.{:08d}.pcd"
         DataName_fmt = os.path.join(data_dir,fname_fmt)
